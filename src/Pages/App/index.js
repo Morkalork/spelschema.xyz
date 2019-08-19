bd3286f485e503885a145bb7bd52b974b3a0d1a8
Magnus Ferm <maffelu@gmail.com>
2019-08-19 20:28:41 +0200

    Added react-router, swiping and a news-section

--- src/Pages/App/index.js
+++ src/Pages/App/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
-import { FixtureProvider } from '../../Contexts/fixture-context';
-import { FixtureList } from '../../Components/FixtureList';
+import {FixtureProvider} from '../../Contexts/fixture-context';
+import {FixtureList} from '../../Components/FixtureList';
 import {Header} from '../../Components/Header';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import News from '../News';
+import SwipeableRoutes from "react-swipeable-routes";
 
 const Wrapper = styled.div`
   min-height: 100%;
@@ -10,6 +13,12 @@ const Wrapper = styled.div`
   padding: 1rem;
   color: #fff;
   text-shadow: 0 0 0.25rem #bbb;
+  display: flex;
+  flex-direction: column;
+  
+  > div {
+    flex: 1
+  }
 `;
 
 function App() {
@@ -17,7 +26,12 @@ function App() {
     <Wrapper>
       <FixtureProvider>
         <Header/>
-        <FixtureList />
+        <Router>
+          <SwipeableRoutes>
+            <Route path='/' exact component={FixtureList}/>
+            <Route path='/news' component={News}/>
+          </SwipeableRoutes>
+        </Router>
       </FixtureProvider>
     </Wrapper>
   );
