7fa46653f40c6ae965fcdce623a122725b5f03ed
Magnus Ferm <maffelu@gmail.com>
2019-08-24 20:42:44 +0200

    Fixed desktop compatibility

--- src/Pages/App/index.js
+++ src/Pages/App/index.js
@@ -10,12 +10,41 @@ import {NewsProvider} from '../../Contexts/news-context';
 
 const Wrapper = styled.div`
   min-height: 100%;
-  background-color: #A7D7FF;
   padding: 1rem;
   color: #fff;
   text-shadow: 0 0 0.25rem #bbb;
   display: flex;
   flex-direction: column;
+  max-width: 768px;
+  margin: 0 auto;
+  
+  .flesktop {
+    display: none;
+  }
+  
+  @media screen and (min-width: 769px) {
+    margin: 2rem auto;
+    border: 12px #000 solid;
+    border-radius: 0.5rem;
+    box-shadow: 0 0 1rem #333;
+  
+    .flesktop {
+      background-color: #000;
+      height: 1rem;
+      margin: -1rem;
+      margin-bottom: 3rem;
+      flex: 0;
+      padding: 1rem;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      
+      p:last-child {
+        font-size: 2rem;
+        padding-right: 1rem;   
+      }
+    }
+  }
   
   > div {
     flex: 1
@@ -25,6 +54,10 @@ const Wrapper = styled.div`
 function App() {
   return (
     <Wrapper>
+      <div className='flesktop'>
+        <p>Denna sida är mobilanpassad farfar...</p>
+        <p>●</p>
+      </div>
       <FixtureProvider>
         <NewsProvider>
           <Header/>
