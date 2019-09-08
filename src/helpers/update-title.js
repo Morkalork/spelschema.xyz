f8e7d4dbfa003faf2c5df07deec746fd872c7e4f
Magnus Ferm <maffelu@gmail.com>
2019-09-08 22:28:03 +0200

    Improved Twitter card

--- src/helpers/update-title.js
+++ src/helpers/update-title.js
@@ -2,4 +2,5 @@ export default (newTitle) => {
 
   document.title = newTitle;
   document.querySelector('meta[property="og:title"]').setAttribute('content', newTitle);
+  document.querySelector('meta[name="twitter:title"]').setAttribute('content', newTitle);
 }
