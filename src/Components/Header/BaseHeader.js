0830662b86c5bb8d60ce67a3a585c09f8f4a1ff2
Magnus Ferm <maffelu@gmail.com>
2020-08-15 13:01:13 +0200

    Fixed the header spinner design

--- src/Components/Header/BaseHeader.js
+++ src/Components/Header/BaseHeader.js
@@ -6,7 +6,7 @@ import updateTitle from '../../helpers/update-title';
 
 const Wrapper = styled.header`
   margin-bottom: 1rem;
-  background-color: #A7D7FF;
+  background-color: #a7d7ff;
   padding: 1rem;
   color: #fff;
 `;
@@ -16,6 +16,11 @@ const StyledH1 = styled.header`
   text-transform: uppercase;
   display: flex;
   justify-content: space-between;
+  position: relative;
+`;
+
+const Title = styled.p`
+  max-width: calc(100% - 3rem);
 `;
 
 const Spinner = styled.p`
@@ -27,8 +32,17 @@ const Spinner = styled.p`
       transform: rotate(-359deg);
     }
   }
-  
-  display: inline-block;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 1rem;
+  right: 1rem;
+  width: 2rem;
+  height: 2rem;
+  line-height: 2rem;
+  font-size: 2rem;
   &.updating {
     animation: spinner 4s linear infinite;
   }
@@ -38,25 +52,33 @@ const SmallParagraph = styled.p`
   font-size: 0.75rem;
 `;
 
-const BaseHeader = ({headerText, isUpdating, reload, lastCheck}) => {
-    const title = `${headerText} ** Malmö FF`;
-    updateTitle(title);
-    return (<Wrapper>
-        <StyledH1><p>{title}</p> {isUpdating !== undefined && <Spinner className={isUpdating ? 'updating' : ''}
-                                                                       onClick={reload}>⟲</Spinner>}</StyledH1>
-        <SmallParagraph>
-            Senast uppdaterad:
-            {isUpdating && `Hämtar data...`}
-            {!isUpdating && moment(lastCheck).format("YYYY-MM-DD HH:mm:ss")}
-        </SmallParagraph>
-    </Wrapper>);
+const BaseHeader = ({ headerText, isUpdating, reload, lastCheck }) => {
+  const title = `${headerText} ** Malmö FF`;
+  updateTitle(title);
+  return (
+    <Wrapper>
+      <StyledH1>
+        <Title>{title}</Title>{' '}
+        {isUpdating !== undefined && (
+          <Spinner className={isUpdating ? 'updating' : ''} onClick={reload}>
+            ⟲
+          </Spinner>
+        )}
+      </StyledH1>
+      <SmallParagraph>
+        Senast uppdaterad:&nbsp;
+        {isUpdating && `Hämtar data...`}
+        {!isUpdating && moment(lastCheck).format('YYYY-MM-DD HH:mm:ss')}
+      </SmallParagraph>
+    </Wrapper>
+  );
 };
 
 BaseHeader.propTypes = {
-    headerText: PropTypes.string.isRequired,
-    isUpdating: PropTypes.bool,
-    reload: PropTypes.func,
-    lastCheck: PropTypes.number
+  headerText: PropTypes.string.isRequired,
+  isUpdating: PropTypes.bool,
+  reload: PropTypes.func,
+  lastCheck: PropTypes.number,
 };
 
 export default BaseHeader;
