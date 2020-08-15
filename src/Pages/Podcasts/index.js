fcbfdebadca12272276e73eaf1a33e94ff796353
Magnus Ferm <maffelu@gmail.com>
2020-08-15 11:22:23 +0200

    Upgraded pod casts and added fixture table link

--- src/Pages/Podcasts/index.js
+++ src/Pages/Podcasts/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import {PodcastSection} from "../../Components/PodcastSection";
+import { PodcastSection } from '../../Components/PodcastSection';
 
 const Wrapper = styled.div`
   h2 {
@@ -9,68 +9,98 @@ const Wrapper = styled.div`
   }
 `;
 
-const mffPods = [{
+const mffPods = [
+  {
     title: 'Blått en podd',
     href: 'https://poddtoppen.se/podcast/1039767398/blatt-en-podd',
-    description: 'SvenskaFans podcast med subjektiv information om nuläget.'
-}, {
+    description: 'SvenskaFans podcast med subjektiv information om nuläget.',
+  },
+  {
     title: 'Blått snack',
     href: 'https://poddtoppen.se/podcast/1085802878/blatt-snack',
-    description: 'Ole Törner och Skånesporten snackar skit och fotboll, ofta med någon intressant gäst!'
-}, {
+    description:
+      'Ole Törner och Skånesporten snackar skit och fotboll, ofta med någon intressant gäst!',
+  },
+  {
     title: 'MFF-podden',
     href: 'https://poddtoppen.se/podcast/911337512/mff-podden',
-    description: 'Wiman och Sydsvenskan ger sin syn på allt omkring Malmö FF'
-}, {
+    description: 'Wiman och Sydsvenskan ger sin syn på allt omkring Malmö FF',
+  },
+  {
     title: 'Norra läktaren',
     href: 'https://poddtoppen.se/podcast/1278732007/norra-laktaren-podden',
-    description: 'Gustav och Victor drar anekdoter och annat om MFF, bortaresor och supporterlivet'
-}, {
-    title: 'Vårt blåa lag',
-    href: 'https://poddtoppen.se/podcast/1252795901/vart-blaa-lag',
-    description: 'Supporterpodcast av supportrar för supportrar'
-}, {
+    description:
+      'Gustav och Victor drar anekdoter och annat om MFF, bortaresor och supporterlivet',
+  },
+  {
+    title: 'Frustrationspodden',
+    href: 'https://poddtoppen.se/podcast/1516464835/frustrationspodden',
+    description:
+      'Fotbollskånes mff/hif pod med separata avsnitt för de olika lagen',
+  },
+  {
     title: 'Tapper',
     href: 'https://poddtoppen.se/podcast/1434984950/tapper',
-    description: 'En pod av Mikael Sjöblom (om Malmö FF)'
-}];
+    description: 'En pod av Mikael Sjöblom (om Malmö FF)',
+  },
+  {
+    title: 'För alltid nummer 1',
+    href: 'https://anchor.fm/for-alltid-nummer-ett',
+    description:
+      'En supporterpodd med Martin och Gustav som kommer beröra det minsta och det mesta gällande Malmö FF.',
+  },
+];
 
-const allsvenskanPods = [{
+const allsvenskanPods = [
+  {
     title: 'Studio Allsvenskan',
     href: 'https://poddtoppen.se/podcast/1241871873/studio-allsvenskan',
-    description: 'Sveriges (tydligen) största podcast om Allsvensk fotboll. Av bl.a. Marcus Birro.'
-}, {
-    title: 'Alltid Allsvenskan',
-    href: 'https://poddtoppen.se/podcast/1364474466/alltid-allsvenskan-med-jonas-dahlquist-mikael-stahre',
-    description: 'Jonas Dahlquist och Stahre snackar allsvenskan. Nytt avsnitt varje tisdag efter allsvensk omgång'
-}, {
-    title: '3-5-2',
-    href: 'https://poddtoppen.se/podcast/1220605973/3-5-2',
-    description: 'Fjäll, Bachner och ett par nissar pratar fotboll med fokus på Allsvenskan'
-}, {
+    description:
+      'Sveriges (tydligen) största podcast om Allsvensk fotboll. Av bl.a. Marcus Birro.',
+  },
+  {
+    title: 'Allsvenska podden',
+    href: 'https://poddtoppen.se/podcast/1095805585/allsvenska-podden',
+    description:
+      'Robert Laul och Per Bohman i frontlinjen för Sportbladets allsvenska podcast',
+  },
+  {
     title: 'Disco & Noa',
     href: 'https://poddtoppen.se/podcast/1457545266/disco-noa',
-    description: 'Kristoffersson och Bachner snackar Allsvenskan'
-}, {
+    description: 'Kristoffersson och Bachner snackar Allsvenskan',
+  },
+  {
     title: 'Ljugarbänken',
     href: 'https://podcasts.nu/poddar/ljugarbanken',
-    description: 'Bergman, Lindström och Edman pratar allsvenskan (sponsrad av ett bettingbolag)'
-}];
+    description:
+      'Bergman, Lindström och Edman pratar allsvenskan (sponsrad av ett bettingbolag)',
+  },
+];
 
 const Podcasts = () => {
-    return <Wrapper className='podcasts'>
-        <h2>Rent MFF-relaterat</h2>
-        {mffPods.map((pod, index) => <PodcastSection key={index}
-                                                     title={pod.title}
-                                                     href={pod.href}
-                                                     description={pod.description}/>)}
+  return (
+    <Wrapper className="podcasts">
+      <h2>Rent MFF-relaterat</h2>
+      {mffPods.map((pod, index) => (
+        <PodcastSection
+          key={index}
+          title={pod.title}
+          href={pod.href}
+          description={pod.description}
+        />
+      ))}
 
-        <h2>Allsvenskan och kanske relaterat</h2>
-        {allsvenskanPods.map((pod, index) => <PodcastSection key={index}
-                                                             title={pod.title}
-                                                             href={pod.href}
-                                                             description={pod.description}/>)}
-    </Wrapper>;
+      <h2>Allsvenskan och kanske relaterat</h2>
+      {allsvenskanPods.map((pod, index) => (
+        <PodcastSection
+          key={index}
+          title={pod.title}
+          href={pod.href}
+          description={pod.description}
+        />
+      ))}
+    </Wrapper>
+  );
 };
 
 export default Podcasts;
--- src/Pages/Table/index.js
+++ src/Pages/Table/index.js
@@ -1,16 +1,29 @@
-import React, {useContext} from 'react';
-import {TableContext} from '../../Contexts/table-context';
+import React, { useContext } from 'react';
+import { TableContext } from '../../Contexts/table-context';
 import Row from './Row';
 import Information from '../../Components/Information';
 
 const Table = () => {
-  const {tableRows} = useContext(TableContext);
-  return <div className='table'>
-    {tableRows.map((tableRow) => <Row tableRow={tableRow} key={tableRow.team}/>)}
-    <Information>
-      Du kan se mer info om du klickar på ett lag i tabellen.
-    </Information>
-  </div>;
+  const { tableRows } = useContext(TableContext);
+  return (
+    <div className="table">
+      {tableRows.map((tableRow) => (
+        <Row tableRow={tableRow} key={tableRow.team} />
+      ))}
+      <Information>
+        Du kan se mer info om du klickar på ett lag i tabellen eller gå till den
+        officiella&nbsp;
+        <a
+          href="https://www.allsvenskan.se/tabell"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          tabellen
+        </a>
+        .
+      </Information>
+    </div>
+  );
 };
 
 export default Table;
